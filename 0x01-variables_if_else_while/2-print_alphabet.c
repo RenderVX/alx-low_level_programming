6e080262346213c2d6b3a651c7536e2faf0993cd
Render <quantumphotonray@gmail.com>
2023-07-31 13:16:30 -0700

    msg

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+
+/**
+* main -  does smth
+* Return: integer
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+for (int i = 97; i < 123; i++)
+{
+putchar(i);
+}
+putchar(10);
+
+}
