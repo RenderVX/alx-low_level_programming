45a39c082ec9e304c175f2a2c50e66bc00a99f68
Render <quantumphotonray@gmail.com>
2023-07-31 13:11:21 -0700

    msg

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+
+/**
+* main -  does smth
+* Return: integer
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+int l;
+l = n % 10;
+
+printf("Last digit of %i is %i and is ", n, l);
+if (l > 5)
+{
+printf("greater than 5");
+}
+else if (l == 0)
+{
+printf("0");
+}
+else
+{
+printf("less than 6 and not 0");
+}
+printf("\n");
+
+
+	/* your code goes there */
+return (0);
+}
