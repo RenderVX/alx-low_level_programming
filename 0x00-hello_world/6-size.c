5b903e25096f2bfe55436da7a1f60882dab1236d
Render <quantumphotonray@gmail.com>
2023-07-31 06:05:23 -0700

    msg

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main -  does smth
+* Return: integer
+*/
+
+
+
+int main(void)
+{
+
+int a,b,c,d,e;
+a = sizeof(char);
+b = sizeof(int)
+c = sizeof(long int);
+d = sizeof(long long int);
+e = sizeof(float);
+  
+
+
+printf("Size of a char: %i byte(s) \n Size of an int: %i byte(s) \n Size of a long int: %i byte(s) \n",a,b,c)
+printf("Size of a long long int: %i byte(s) \n Size of a float: %i byte(s)\n",d,e)
+
+
+
+}
