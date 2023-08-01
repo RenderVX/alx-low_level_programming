ee2e9dfc7f9e3475393238da4cee15661fe53270
Render <quantumphotonray@gmail.com>
2023-07-31 23:50:57 -0700

    msg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+
+/**
+* main -  does smth
+* Return: integer
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+}
