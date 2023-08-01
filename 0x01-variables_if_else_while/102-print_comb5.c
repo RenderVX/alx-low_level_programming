7f0520a8ad54db1a3b70991fdb11a1d6d92ccd8c
Render <quantumphotonray@gmail.com>
2023-08-01 05:39:06 -0700

    msg

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main -  does smth
+ * Return: integer
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i, j, k, l;
+for (i = 48; i < 58; i++)
+{
+for (j = 48; j < 58; j++)
+{
+for (k = 48; k < 58; k++)
+{
+for (l = 48; l < 58; l++)
+{
+if (i > k || j >= l)
+{
+continue;
+}
+putchar(i);
+putchar(j);
+putchar(32);
+putchar(k);
+putchar(l);
+if (i == 57 && j == 56 && k == 57 && l == 57)
+{
+break;
+}
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
