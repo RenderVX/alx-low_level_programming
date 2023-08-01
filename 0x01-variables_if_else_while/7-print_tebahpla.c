d4333cfbfc69752eaec5614328ff0665d93871e3
Render <quantumphotonray@gmail.com>
2023-08-01 02:14:14 -0700

    msg

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+
+/**
+* main -  does smth
+* Return: integer
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i;
+for (i = 122; i > 96; i--)
+{
+putchar(i);
+}
+putchar(20);
+return (0);
+}
